cf00541903f6e39e3db99f816ef18154dbddf0bb
Sylwek <sylweradoy@wp.pl>
2025-10-05 16:04:21 +0200

    Dodaje opinie a anti botem

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -5,6 +5,9 @@ import About from "./components//About/About";
 import Process from "./components/Process/Process";
 import Gallery  from "./components/Gallery/Gallery";
 import Contact from "./components/Contact/Contact";
+import Reviews from "./components/Reviews/Reviews";
+
+
 
 
 export default function Page() {
@@ -16,6 +19,7 @@ export default function Page() {
     <Process />
      <Gallery />
       <Contact />
+      <Reviews />
     </>
   );
 }
